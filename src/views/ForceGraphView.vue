284462720f4136e485ff2ef5a11f8cda733ceab5
JulienTom33 <jul.tom@hotmail.fr>
2023-07-28 11:38:35 +0200

    CREATE: forcegraph view , cytoscape view, and easy navigation through the pages

---
+++ src/views/ForceGraphView.vue
@@ -0,0 +1,12 @@
+<script setup>    </script>
+
+<template>
+    <div>
+        <router-link to="/">Go to Home</router-link> |
+        <router-link to="/cytoscape">Go to Cytoscape</router-link> |
+        <router-link to="/forcegraph">Go to Forcegraph</router-link> 
+    </div>
+    <h1>Forcegraph</h1>
+</template>    
+
+<style scoped></style>
