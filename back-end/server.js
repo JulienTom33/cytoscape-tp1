877dbebbd66b7ac4c04fb9127018fe182c9cea8c
JulienTom33 <jul.tom@hotmail.fr>
2023-08-01 10:48:20 +0200

    UPDATE: cytosnap configured

--- back-end/server.js
+++ back-end/server.js
@@ -21,9 +21,7 @@ var options = {
     args: ['--no-sandbox', '--disable-setuid-sandbox'] // Add any other Puppeteer arguments here as needed
   }
 };
-const snap = cytosnap(options); // Utiliser les options lors de l'initialisation de Cytosnap
 
-// Fonction pour lire le contenu du fichier JSON et le convertir en un objet de graphe
 const readAndParseGraphJSON = async (filePath) => {
   try {
     const fileContent = await fs.promises.readFile(filePath, 'utf-8');
@@ -36,18 +34,212 @@ const readAndParseGraphJSON = async (filePath) => {
   }
 };
 
+const graphElements = (graphData) => {
+  const data = graphData;
+  let nodes = [];
+  let edges = [];
+  if (data.nodes && data.edges) {
+    nodes = data.nodes;
+    edges = data.edges;
+  } else if (data.elements && Array.isArray(data.elements)) {  
+    nodes = data.elements.filter((element) => element.group === 'nodes');
+    edges = data.elements.filter((element) => element.group === 'edges');
+  } else if (data.elements && data.elements.nodes && data.elements.edges) {
+    nodes = data.elements.nodes;
+    edges = data.elements.edges;
+  }
+  return { nodes, edges };
+};
+
+const layoutOptions = {
+  preset: {
+    name: 'preset',
+    animate: false,
+    fit: true,
+    animationDuration: 500,
+
+  },
+  null: {
+    name: 'null',
+    animate: false,
+    fit: true,
+  },
+  random: {
+    name: 'random',
+    animate: false,
+    fit: true,
+  },
+  grid: {
+    name: 'grid',
+    animate: false,
+    fit: true,
+    animationDuration: 500,    
+    // uses all available space on false, uses minimal space on true
+    condense: false,
+    // force num of rows in the grid
+    rows: 4, 
+    // force num of columns in the grid 
+    cols: 5, 
+  },
+  circle: {
+    name: 'circle',
+    animate: false,
+    fit: true,
+    // prevents node overlap, may overflow boundingBox and radius if not enough space
+    avoidOverlap: true, 
+    // Excludes the label when calculating node bounding boxes for the layout algorithm
+    nodeDimensionsIncludeLabels: false,
+    // the radius of the circle 
+    radius: undefined,
+    // whether the layout should go clockwise (true) or counterclockwise/anticlockwise (false) 
+    clockwise: true, 
+  },
+  concentric: {
+    name: 'concentric',
+    animate: false,
+    fit: true,
+    // where nodes start in radians
+    startAngle: 3 / 2 * Math.PI, 
+    // whether the layout should go clockwise (true) or counterclockwise/anticlockwise (false)
+    clockwise: true,
+    // whether levels have an equal radial distance betwen them, may cause bounding box overflow 
+    equidistant: false,
+    // min spacing between outside of nodes (used for radius adjustment) 
+    minNodeSpacing: 10, 
+    // height of layout area (overrides container height)
+    height: undefined, 
+    // width of layout area (overrides container width)
+    width: undefined, 
+  },
+  breadthfirst: {
+    name: 'breadthfirst',
+    animate: false,
+    fit: true,
+    // whether the tree is directed downwards (or edges can point in any direction if false)
+    directed: false, 
+    // put depths in concentric circles if true, put depths top down if false
+    circle: false, 
+    // whether to create an even grid into which the DAG is placed (circle:false only)
+    grid: false, 
+    // positive spacing factor, larger => more space between nodes (N.B. n/a if causes overlap)
+    spacingFactor: 1.75, 
+    // prevents node overlap, may overflow boundingBox if not enough space
+    avoidOverlap: true,
+    animationDuration: 500
+  },
+  dagre: {
+    name: 'dagre',
+    animate: false,
+    fit: true,
+  },
+  klay: {
+    name: 'klay',
+    animate: false,
+    fit: true,
+  },
+  cose: {
+    name: 'cose',
+    animate: false,
+    fit: true,
+    animationDuration: undefined,
+    // Number of iterations between consecutive screen positions update
+    refresh: 20,
+    // Excludes the label when calculating node bounding boxes for the layout algorithm
+    nodeDimensionsIncludeLabels: false,
+    // Randomize the initial positions of the nodes (true) or use existing positions (false)
+    randomize: false,
+    // Extra spacing between components in non-compound graphs
+    componentSpacing: 40,
+    // Node repulsion (overlapping) multiplier
+    nodeOverlap: 4,
+    // Maximum number of iterations to perform
+    numIter: 1000,
+  },
+  'cose-bilkent': {
+    name: 'cose-bilkent',
+    animate: false,
+    fit: true,
+    padding: 10,
+    // 'draft', 'default' or 'proof" 
+    // - 'draft' fast cooling rate 
+    // - 'default' moderate cooling rate 
+    // - "proof" slow cooling rate
+    quality: 'default',   
+    // number of ticks per frame; higher is faster but more jerky
+    refresh: 30,   
+    // Whether to enable incremental mode
+    randomize: true, 
+    // Ideal (intra-graph) edge length
+    idealEdgeLength: 50,
+    // Divisor to compute edge forces
+    edgeElasticity: 0.45,
+    // Nesting factor (multiplier) to compute ideal edge length for inter-graph edges
+    nestingFactor: 0.1,    
+    // Maximum number of iterations to perform
+    numIter: 2500,
+    // Whether to tile disconnected nodes
+    tile: true,
+    // Type of layout animation. The option set is {'during', 'end', false}
+    animate: 'end',
+    // Duration for animate:end
+    animationDuration: 500,    
+  },
+  cola: {
+    name: 'cola',
+    animate: false,
+    fit: true,
+  },
+  euler: {
+    name: 'euler',
+    animate: true,
+    fit: true, 
+    // Friction / drag coefficient to make the system stabilise over time
+    dragCoeff: 0.02,
+    // Whether to randomize the initial positions of the nodes
+    // true : Use random positions within the bounding box
+    // false : Use the current node positions as the initial positions
+    randomize: false,
+    // The amount of time passed per tick
+    // - Larger values result in faster runtimes but might spread things out too far
+    // - Smaller values produce more accurate results
+    timeStep: 10,
+    // The number of ticks per frame for animate:true
+    // - A larger value reduces rendering cost but can be jerky
+    // - A smaller value increases rendering cost but is smoother
+    refresh: 10,
+    // Maximum iterations and time (in ms) before the layout will bail out
+    // - A large value may allow for a better result
+    // - A small value may make the layout end prematurely
+    // - The layout may stop before this if it has settled
+    maxIterations: 2000,
+    maxSimulationTime: 7000,
+    // Prevent the user grabbing nodes during the layout (usually with animate:true)
+    ungrabifyWhileSimulating: false,
+  },
+  spread: {
+    name: 'spread',
+    animate: false,
+    fit: true,
+  } 
+};
+
+
 const generateGraphImage = async (graphData) => {
   try {
-    const snap = cytosnap();
+    const snap = cytosnap(options);
     await snap.start();
+
+    const { nodes, edges } = graphElements(graphData);
 
     const img = await snap.shot({
       elements: {
-        nodes: graphData.elements.nodes,
-        edges: graphData.elements.edges
+        // nodes: graphData.elements.nodes,
+        // edges: graphData.elements.edges
+        nodes: nodes,
+        edges: edges
       },
       layout: {
-                name: 'preset' // you may reference a `cytoscape.use()`d extension name here
+                name: 'preset'                
               },
       style: [
                 {
@@ -65,8 +257,8 @@ const generateGraphImage = async (graphData) => {
       ],
       resolvesTo: 'base64uri',
       format: 'png',
-      width: 1024,
-      height: 768,
+      width: 640,
+      height: 480,
       background: 'transparent'
     });
 
