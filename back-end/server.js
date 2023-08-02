8d45fb03705b3a6a96b84702205e13be0e241c4c
JulienTom33 <jul.tom@hotmail.fr>
2023-08-02 14:28:04 +0200

    clean code

--- back-end/server.js
+++ back-end/server.js
@@ -182,13 +182,14 @@ app.get('/api/files', (req, res) => {
 // CYTOSCAPE SERVER TEST
 
 // Chemin du fichier JSON dans le dossier "files"
-const filePath = path.join(__dirname, 'files', 'soplon.json');
+const filePath = path.join(__dirname, 'files', 'nba-20.json');
+
 
 // Route pour récupérer le graphe généré avec Cytoscape depuis le fichier JSON
 app.get('/api/graph', (req, res) => {
   try {
     const graphData = createGraphFromJSON(filePath);
-    console.log(graphData);
+    // console.log(graphData);
     res.json(graphData);
   } catch (error) {
     console.error(error);
@@ -198,29 +199,30 @@ app.get('/api/graph', (req, res) => {
 
 // Fonction pour créer le graphe avec Cytoscape à partir du fichier JSON
 function createGraphFromJSON(filePath) {
-  const cy = cytoscape({ headless: true });
+  const cy = cytoscape({ headless: true }); 
 
   try {
     const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    // console.log(jsonData)
     let nodes = [];
     let edges = [];
 
-    // Vérifier que le fichier JSON contient les propriétés 'nodes' et 'edges'
-    if (!jsonData.nodes || !jsonData.edges) {
-      throw new Error('Le fichier JSON doit contenir les propriétés "nodes" et "edges".');
+    
+    if (Array.isArray(jsonData)) {
+      nodes = jsonData.filter((element) => element.group === 'nodes');
+      edges = jsonData.filter((element) => element.group === 'edges');
+    } else {     
+      if (!jsonData.elements || !jsonData.elements.nodes || !jsonData.elements.edges) {
+        throw new Error('Le fichier JSON doit contenir les propriétés "elements", "nodes" et "edges".');
+      }
+
+      nodes = jsonData.elements.nodes;
+      edges = jsonData.elements.edges;
     }
-
-    nodes = jsonData.nodes;
-    edges = jsonData.edges;
-
-    // Ajouter les nœuds et les arêtes au graphe
+   
     cy.add(nodes.concat(edges));
-
-    // Appliquer le layout pour que les nœuds soient bien positionnés
+    
     cy.layout({ name: 'preset' }).run();
-
-    // Récupérer les éléments du graphe sous forme de données JSON
+   
     const graphData = cy.json().elements;
 
     return graphData;
@@ -228,75 +230,7 @@ function createGraphFromJSON(filePath) {
     console.error('Erreur lors de la création du graphe à partir du fichier JSON:', error);
     throw error;
   }
-
-  // try {
-  //   const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-  //   let nodes = [];
-  //   let edges = [];
-
-    
-  //   if (Array.isArray(jsonData)) {
-  //     nodes = jsonData.filter((element) => element.group === 'nodes');
-  //     edges = jsonData.filter((element) => element.group === 'edges');
-  //   } else {     
-  //     if (!jsonData.elements || !jsonData.elements.nodes || !jsonData.elements.edges) {
-  //       throw new Error('Le fichier JSON doit contenir les propriétés "elements", "nodes" et "edges".');
-  //     }
-
-  //     nodes = jsonData.elements.nodes;
-  //     edges = jsonData.elements.edges;
-  //   }
-   
-  //   cy.add(nodes.concat(edges));
-    
-  //   cy.layout({ name: 'grid' }).run();
-   
-  //   const graphData = cy.json().elements;
-
-  //   return graphData;
-  // } catch (error) {
-  //   console.error('Erreur lors de la création du graphe à partir du fichier JSON:', error);
-  //   throw error;
-  // }
 }
-
-// // Route pour récupérer le graphe généré avec Cytoscape
-// app.get('/api/graph', (req, res) => { 
-//   const graphData = createGraph();
-//   res.json(graphData);
-// });
-
-// // Fonction pour créer le graphe avec Cytoscape
-// function createGraph() {
-//   const cy = cytoscape();
-
-//   // Ajouter des nœuds au graphe
-//   cy.add([
-//     { data: { id: 'node1', label: 'Node 1' } },
-//     { data: { id: 'node2', label: 'Node 2' } },
-//     { data: { id: 'node3', label: 'Node 3' } },
-//     { data: { id: 'node4', label: 'Node 4' } },
-//     { data: { id: 'node5', label: 'Node 5' } },
-//     // Ajoutez d'autres nœuds ici...
-//   ]);
-
-//   // Ajouter des arêtes au graphe
-//   cy.add([
-//     { data: { source: 'node1', target: 'node2' } },
-//     { data: { source: 'node2', target: 'node3' } },
-//     { data: { source: 'node1', target: 'node4' } },
-//     { data: { source: 'node2', target: 'node5' } },
-//     // Ajoutez d'autres arêtes ici...
-//   ]);
-
-//   // Appliquer le layout pour que les nœuds soient bien positionnés
-//   cy.layout({ name: 'grid' }).run();
-
-//   // Récupérer les éléments du graphe sous forme de données JSON
-//   const graphData = cy.json().elements;
-
-//   return graphData;
-// }
 
 // ****************************************************************************************************************************************
 /* 
