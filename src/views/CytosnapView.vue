d8623b1765daccf77470a994a325dad6bed29ee2
JulienTom33 <jul.tom@hotmail.fr>
2023-08-02 10:24:33 +0200

    WIP: Cytosnap view: attempts

--- src/views/CytosnapView.vue
+++ src/views/CytosnapView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue'
+import { ref, onMounted, computed, reactive } from 'vue'
 import axios from 'axios';
 
 const getAllFiles = async () => {
@@ -53,8 +53,67 @@ const zoomStyle = computed(() => ({
   transform: `scale(${zoomLevel.value / 100})`,
 }));
 
+// const scale = ref(1);
+// const panning = ref(false);
+// const pointX = ref(0);
+// const pointY = ref(0);
+// const start = reactive({ x: 0, y: 0 });
+// const translateX = ref(0);
+// const translateY = ref(0);
+// const originX = ref(0);
+// const originY = ref(0);
 
-onMounted(async ()=>{  
+// const zoomRef = ref(null);
+
+// const setTransform = () => {
+//   zoomRef.value.style.transform = `scale(${scale.value}) translate(${translateX.value}px, ${translateY.value}px)`;
+// };
+
+// function handleMouseDown(e) {
+//   e.preventDefault();
+//   start.x = e.clientX - translateX.value;
+//   start.y = e.clientY - translateY.value;
+//   panning.value = true;
+// }
+
+// function handleMouseMove(e) {
+//   e.preventDefault();
+//   if (!panning.value) {
+//     return;
+//   }
+
+//   translateX.value = e.clientX - start.x;
+//   translateY.value = e.clientY - start.y;
+//   setTransform();
+// }
+
+// function handleWheel(e) {
+//   e.preventDefault();
+//   const xs = (e.clientX - translateX.value) / scale.value;
+//   const ys = (e.clientY - translateY.value) / scale.value;
+//   const delta = (e.wheelDelta ? e.wheelDelta : -e.deltaY);
+
+//   if (delta > 0) {
+//     scale.value *= 1.2;
+//   } else {
+//     scale.value /= 1.2;
+//   }
+
+//   const newX = e.clientX - xs * scale.value;
+//   const newY = e.clientY - ys * scale.value;
+
+//   originX.value = newX - translateX.value;
+//   originY.value = newY - translateY.value;
+
+//   translateX.value = newX;
+//   translateY.value = newY;
+
+//   setTransform();
+// }
+
+
+
+onMounted(async ()=>{   
     loadGraphImage(); 
     getAllFiles()      
   }); 
@@ -105,9 +164,14 @@ onMounted(async ()=>{
       </select> -->
     </header> 
     
- <div @wheel="handleMouseWheel">
-      <img :src="graphImage" alt="Graph" :style="zoomStyle" class="graphImage" />
-      <div id="cy"></div>
+    <div @wheel="handleMouseWheel">
+      <!-- <div  ref="zoomRef"
+        @wheel="handleWheel"
+        @mousedown="handleMouseDown"
+        @mousemove="handleMouseMove" id="zoom"> -->
+      
+      <img :src="graphImage" alt="Graph" :style="zoomStyle" class="graphImage" />     
+      <!-- <img :src="graphImage" alt="Graph" class="graphImage" :style="{ transform: `scale(${scale}) translate(${pointX}px, ${pointY}px)` }" />      -->
     </div>
 
     
