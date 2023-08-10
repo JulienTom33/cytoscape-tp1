d90057bef61f04a2ccb671fdcfc2e941e702ca22
JulienTom33 <jul.tom@hotmail.fr>
2023-08-10 10:25:54 +0200

    vercel test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,10 @@ import { RouterLink } from 'vue-router';
 
 <template>
     <div style="font-size: 2em; display: flex; border: 1px dashed black; border-radius: 5px; background-color: antiquewhite; padding: 10px;">
-      <router-link to="https://cytoscape-tp1.vercel.app/">Go to Home</router-link> 
-      <router-link to="https://cytoscape-tp1.vercel.app/cytoscape">Go to Cytoscape</router-link>
-      <router-link to="https://cytoscape-tp1.vercel.app/cytosnap">Go to Cytosnap</router-link>  
-      <router-link to="https://cytoscape-tp1.vercel.app/cytoscape-server">Go to CytoscapeServer</router-link> 
+      <router-link to="/">Go to Home</router-link> 
+      <router-link to="/cytoscape">Go to Cytoscape</router-link>
+      <router-link to="/cytosnap">Go to Cytosnap</router-link>  
+      <router-link to="/cytoscape-server">Go to CytoscapeServer</router-link> 
     </div>
     
 </template>
